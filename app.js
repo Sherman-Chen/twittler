7afc8d557d87490c9d976437499701fec7948783
Sherman <shc410@gmail.com>
2016-09-03 22:24:23 -0700

    Make some progress with refresh button

--- app.js
+++ app.js
@@ -17,21 +17,27 @@
 
 $(document).ready(function() {
   
-  function tweet() {
+  var tweet = function() {
 
   var index = streams.home.length - 1;
-  var tweet = streams.home[index];
 
-  while (index >=0) {
+  while (index >= 0) {
+    var tweet = streams.home[index];
     var $tweet = $('<div></div>');
-    $tweet.html('<div class="tweet"> @' + tweet.user + ': ' + tweet.message + '</div>');
+    $tweet.html('<div class="tweet"> @' + '<span class="user" data-user="' + tweet.user + '">' + tweet.user + '</span>: ' 
+      + tweet.message + '<span class="time-stamp">' + tweet.created_at + '</span>'  + '</div>');
     $tweet.prependTo($('.feed'));
     index -= 1;
-  };
+  }
+
+  $('.user').on('click', function() {
+    
+  });
+
 }
 
-  $('.refresh-btn').on('click', tweet);
-
   tweet();
+
+  $('.refresh-btn').on('click', tweet);
   
 });

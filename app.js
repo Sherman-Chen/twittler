73286bda54f4263be8647f5b00b94c2cf6cc0cb1
Sherman <shc410@gmail.com>
2016-09-05 02:38:50 -0700

    Finish up and cleanup

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 $(document).ready(function() {
-  
-  function makeTweet() {
 
-  var index = streams.home.length - 1;
-  var $feed = $('.feed');
+  function makeTweet(user) {
+    var $feed = $('.feed');
+    var index = streams.home.length - 1;
 
-  while (index >= 0) {
-    var tweet = streams.home[index];
-    
-    var $tweet = $('<div></div>');
-    $tweet.html('<div class="tweet"> @ <span class="user" data-user="' + tweet.user + '">' + tweet.user + '</span>: ' 
-      + tweet.message + '<span class="time-stamp">' + tweet.created_at + '</span>' + "TESTING: " + index  + '</div>');
-    $tweet.prependTo($feed);
-    
-    index -= 1;
-  }
+    $feed.html('');
+    while (index >= 0) {
+      var tweet = streams.home[index];
+      
+      if (user === undefined) {
+        tweetMessage();
+      } else if (user === tweet.user) {
+        tweetMessage();
+      }
+      index -= 1;
+    }
 
-  $('.user').on('click', function() {
-    console.log('success');
-  });
+    $('.user').on('click', function() {
+      makeTweet($(this).data("user"))
+    });
 
+    function tweetMessage() {
+      var $tweet = $('<div></div>');
+      $tweet.html('<div class="tweet"> @<span class="user" data-user="' + tweet.user + '">' + tweet.user + '</span>: ' 
+        + tweet.message + '<span class="time-stamp">' + tweet.created_at + '</span>' + '</div>');
+      $tweet.appendTo($feed);
+    }
 }
 
   makeTweet();
 
-  $('.refresh-btn').on('click', makeTweet);
+  $('.refresh-btn').on('click', function() {
+    makeTweet();
+  });
   
 });

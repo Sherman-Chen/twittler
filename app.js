224e38bcc57f2dff1f14786ffe99abde5ae59229
Sherman <shc410@gmail.com>
2016-09-04 17:40:03 -0700

    Make some minor changes/cleaning up

--- app.js
+++ app.js
@@ -1,43 +1,29 @@
-
-
-      // $(document).ready(function(){
-      //   var $body = $('body');
-      //   $body.html('');
-
-      //   var index = streams.home.length - 1;
-      //   while(index >= 0){
-      //     var tweet = streams.home[index];
-      //     var $tweet = $('<div></div>');
-      //     $tweet.text('@' + tweet.user + ': ' + tweet.message);
-      //     $tweet.appendTo($body);
-      //     index -= 1;
-      //   }
-
-      // });
-
 $(document).ready(function() {
   
-  var tweet = function() {
+  function makeTweet() {
 
   var index = streams.home.length - 1;
+  var $feed = $('.feed');
 
   while (index >= 0) {
     var tweet = streams.home[index];
+    
     var $tweet = $('<div></div>');
-    $tweet.html('<div class="tweet"> @' + '<span class="user" data-user="' + tweet.user + '">' + tweet.user + '</span>: ' 
-      + tweet.message + '<span class="time-stamp">' + tweet.created_at + '</span>'  + '</div>');
-    $tweet.prependTo($('.feed'));
+    $tweet.html('<div class="tweet"> @ <span class="user" data-user="' + tweet.user + '">' + tweet.user + '</span>: ' 
+      + tweet.message + '<span class="time-stamp">' + tweet.created_at + '</span>' + "TESTING: " + index  + '</div>');
+    $tweet.prependTo($feed);
+    
     index -= 1;
   }
 
   $('.user').on('click', function() {
-    
+    console.log('success');
   });
 
 }
 
-  tweet();
+  makeTweet();
 
-  $('.refresh-btn').on('click', tweet);
+  $('.refresh-btn').on('click', makeTweet);
   
 });
